3d76361aae9d8d302190e7213f82c3ffeb28b2ff
Sasha <taylork85@gmail.com>
2020-01-24 18:40:13 -0600

    sass set up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { Component, Fragment} from 'react';
-import logo from './logo.svg';
-import AppBar from '@material-ui/core/AppBar';
-import './App.css';
+import {Route, Switch, BrowserRouter} from 'react-router-dom';
+import MarketingPage from './components/MarketingPage';
+import Home from './components/Home.js';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import './App.scss';
 
-class App extends Component {
-  render() {
+const App = () => {
     return (
-      <Fragment>
-        <AppBar color="green" />
-      </Fragment>
+      <MuiThemeProvider>
+        <Fragment>
+        <AppBar title="The V-Spot"/>
+          <BrowserRouter>
+          <Switch>
+          <Route exact path='/market' component={MarketingPage} />
+          <Route exact path='/' component={Home} />   
+          </Switch>
+          </BrowserRouter>
+        </Fragment>
+      </MuiThemeProvider>
     );
-  }
 }
 
 export default App;
---
+++ src/components/MarketingPage.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+const MarketingPage = () => {
+    return(
+        <div>
+            <div>
+                <h1>V-Spot</h1>
+                <button>Order Now</button>
+            </div>
+            <div>
+                <h1>How It Works</h1>
+            </div>
+
+            <div>
+                <div>
+                    <h4>We Cook</h4>
+                    <p>Each day we freshly prepare a variety of delious vegan meals</p>
+                </div>  
+                <div>
+                    <h4>You Order</h4>
+                    <p>Select from a rotating menu of locally-sourced meals. Orders are placed at least 24hours in advance</p>
+                </div>  
+                <div>
+                    <h4>We Deliver</h4>
+                    <p>Meals arrived chilled so everything stays fresh. A few minutes in your oven or microwave and you're ready to eat.</p>
+                </div>  
+            </div>
+
+            <div>
+                <div>
+                    <h1>High Quality Organic Ingredients</h1>
+                </div>
+                <div>
+                    <p>We believe in using the highest quality ingredients that produce positive effects in the body. We salt our dishes with Pink Himalayn salt and only use sunflower or olive oil. </p>
+                    <ul>
+                        <li>No soy</li>
+                        <li>No GMOs</li>
+                        <li>No vegetable oils</li>
+                        <li>No table salt</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <h1>Meals Freshly Prepared Daily</h1>
+                </div>
+                <div>
+                    <p>We make it easy for you to consume fresh, delious, exciting meals that work with your diet</p>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <h1>On Demand Delivery</h1>
+                </div>
+                <div>
+                    <p>Whether you're ordering a meals for the next week or the next day, we're built to work with your schedule, not against it.</p>
+                </div>
+            </div>
+                        
+            <div>
+                <h1>Select a Plan To Save More</h1>
+            </div>
+            <div>
+                <h1>What's On The Menu</h1>
+            </div>
+        </div>    
+    )
+}
+
+export default MarketingPage;
